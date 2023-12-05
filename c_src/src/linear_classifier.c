49fa1777ea7bb741eb24d1f6e0a99026b977e1f2
Alexander <test123erasry4r@test123123.de>
2023-12-05 08:59:07 +0100

    implemented adam

--- c_src/src/linear_classifier.c
+++ c_src/src/linear_classifier.c
@@ -5,18 +5,28 @@
 #include <math.h>
 
 static float weight[NUM_FEATURES] = {1e-6};
-static float bias = 0;
+static float bias = 1.0;
+static float m_t_bias = 0;
+static float v_t_bias = 0;
 static float d_bias = 0;
 static float d_weight[NUM_FEATURES] = {0};
+static float m_t_weight[NUM_FEATURES] = {0};
+static float v_t_weight[NUM_FEATURES] = {0};
 static float features[NUM_FEATURES];
 
-#define LEARNING_RATE (1e-9)
+#define BETA_1 (0.99f)
+#define BETA_2 (0.999f)
+#define EPSILON (1e-8)
+
+#define LEARNING_RATE (1e-5)
+
+#define BATCH_SIZE 1
 
 float classify(float *in)
 {
     float temp = 0;
     for (uint32_t i = 0; i < NUM_FEATURES; i++) {
-        temp += in[i] * weight[i];
+        temp += in[i] / LENGTH_TIME_SERIES * weight[i];
     }
 
     return tanhf(temp + bias);
@@ -24,13 +34,14 @@ float classify(float *in)
 
 static int train_step(float *in, float out)
 {
-    conv_multiple(in, features, get_kernels(), NUM_KERNELS, get_dilations() ,NUM_DILATIONS, get_biases(), NUM_BIASES_PER_KERNEL);
+    conv_multiple(in, features, get_kernels(), NUM_KERNELS, get_dilations(), NUM_DILATIONS, get_biases(), NUM_BIASES_PER_KERNEL);
 
     float out_pred = classify(features);
+    // printf("Features %f, %f, %f\n", features[0], features[1], features[2]);
     for (uint32_t i = 0; i < NUM_FEATURES; i++) {
-        d_weight[i] += LEARNING_RATE * (features[i] * (out - out_pred) * (1 - out_pred*out_pred));
+        d_weight[i] -= (features[i] / LENGTH_TIME_SERIES * (out - out_pred) * (1 - out_pred*out_pred));
     }
-    d_bias += LEARNING_RATE * (out - out_pred) * (1 - out_pred*out_pred);
+    d_bias -= (out - out_pred) * (1 - out_pred*out_pred);
     //printf("out: %f, out_pred: %f\n", out, out_pred);
     
     if (out < 0 && out_pred < 0) {
@@ -48,16 +59,29 @@ void train()
 {
     printf("Starting training\n");
     float accuracy_filtered = 0;
-    const float gamma = 0.01;
+    const float gamma = 0.001;
     for (uint64_t epoch = 0; epoch < 100000; epoch++) {
-        if (epoch % 100 == 99) {
+        if (epoch % BATCH_SIZE == BATCH_SIZE - 1) {
+            float beta_1_pow = powf(BETA_1, (int) (epoch / LENGTH_TIME_SERIES + 1));
+            float beta_2_pow = powf(BETA_2, (int) (epoch / LENGTH_TIME_SERIES + 1));
             for (int i = 0; i < NUM_FEATURES; i++) {
-                weight[i] += d_weight[i];
+                m_t_weight[i] = BETA_1 * m_t_weight[i] + (1 - BETA_1) * d_weight[i];
+                v_t_weight[i] = BETA_2 * v_t_weight[i] + (1 - BETA_2) * d_weight[i] * d_weight[i];
+
+                float mt_hat = m_t_weight[i] / (1 - beta_1_pow);
+                float vt_hat = v_t_weight[i] / (1 - beta_2_pow);
+
+                weight[i] -= LEARNING_RATE * mt_hat / (sqrtf(vt_hat) + EPSILON);
                 d_weight[i] = 0;
             }
-            bias += d_bias;
+            m_t_bias = BETA_1 * m_t_bias + (1 - BETA_1) * d_bias;
+            v_t_bias = BETA_2 * v_t_bias + (1 - BETA_2) * d_bias * d_bias;
+
+            float mt_hat = m_t_bias / (1 - beta_1_pow);
+            float vt_hat = v_t_bias / (1 - beta_2_pow);
+            bias -= LEARNING_RATE * mt_hat / (sqrtf(vt_hat) + EPSILON);
             d_bias = 0;
-            printf("%f\n", accuracy_filtered);
+            printf("Epoch: %u, Accuracy: %f \n", epoch, accuracy_filtered);
         }
         accuracy_filtered = gamma*train_step(get_timeseries()[epoch % NUM_TIMESERIES], get_labels()[epoch % NUM_TIMESERIES]) + (1 - gamma) * accuracy_filtered;
     }
