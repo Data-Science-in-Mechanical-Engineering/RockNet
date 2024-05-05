1ccde4a0fe778645d9ca526953a9ceadbfca31ca
Alexander <test123erasry4r@test123123.de>
2024-05-05 14:41:49 +0200

    temp

--- python_simulation/trainer.py
+++ python_simulation/trainer.py
@@ -60,7 +60,7 @@ def get_dataloader(
 
 def get_accuracy(y_pred, labels):
     _, predicted = torch.max(y_pred, dim=1)
-    printf(labels)
+    printflabels)
     return (predicted == labels).sum() / len(labels)
 
 
